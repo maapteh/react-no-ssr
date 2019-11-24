deae9ddda16c18f02ea4ff067344e42625811490
maapteh <maarten@oudenniel.nl>
2019-11-24 18:31:53 +0100

    remove babel

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-  transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/src.*(\\.|/)(test|spec))\\.(tsx?|ts?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePathIgnorePatterns: ['.next'],
-  collectCoverage: true,
-  coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  globals: {
-      'ts-jest': {
-          diagnostics: false,
-          babelConfig: '.babelrc',
-          tsConfig: './jest.tsconfig.json',
-      },
-  },
-  rootDir: process.cwd(),
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/src.*(\\.|/)(test|spec))\\.(tsx?|ts?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    modulePathIgnorePatterns: ['.next'],
+    collectCoverage: true,
+    coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+    globals: {
+        'ts-jest': {
+            diagnostics: false,
+            tsConfig: './jest.tsconfig.json',
+        },
+    },
+    rootDir: process.cwd(),
 };
